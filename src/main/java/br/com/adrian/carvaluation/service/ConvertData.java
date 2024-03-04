77ab135ac9b681cf6542ee037113ace93faad729
Adrian <adrian.gabriel.dev@gmail.com>
2024-03-04 11:07:48 -0300

    commit:: revisão de código

--- src/main/java/br/com/adrian/carvaluation/service/ConvertData.java
+++ src/main/java/br/com/adrian/carvaluation/service/ConvertData.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class ConvertData implements IConvertData {
 
-    private ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public <T> T getData(String json, Class<T> classe) throws JsonProcessingException {
